e00057b217f9f225f4e4293adab4bf406c1e6952
Iiviii <evelin.zirakashvili1@gmail.com>
2020-10-06 12:38:13 +0400

    creative

---
+++ creative/main.js
@@ -0,0 +1,5 @@
+var search = document.getElementById('search')
+search.addEventListener('click', () =>{
+    var searchArea = document.getElementById('search-area')
+    searchArea.classList.toggle('show');
+})
