805817eaf687b6793a5004ecdd82b477213e850c
Iiviii <evelin.zirakashvili1@gmail.com>
2020-10-12 12:24:55 +0400

    galaxy

--- galaxy/main.js
+++ galaxy/main.js
@@ -2,8 +2,17 @@ AOS.init({
     offset: 200,
     duration: 900
 });
-var search = document.getElementById('search')
-search.addEventListener('click', () =>{
-    var searchArea = document.getElementById('search-area')
-    searchArea.classList.toggle('show');
-})
+$('#search').on('click', function(){
+    $('#search-area').toggle();
+});
+$('#burger').on('click', function(){
+    $(this).toggleClass('active');
+    $('.main-title-area').toggle();
+    $('#search').toggle();
+    $('.nav-ul').toggleClass('none');
+    $('.nav').toggleClass('burger-button');
+    $('.nav-ul').toggleClass('column');
+    $('.nav-li').toggleClass('margin-20px');
+    $('.nav-a').toggleClass('burger-list');
+});
+    
