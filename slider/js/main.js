303e8cbba444fec73a0f6b5ce4617d7e86817e62
Iiviii <evelin.zirakashvili1@gmail.com>
2020-10-28 20:48:55 +0400

    slider

---
+++ slider/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        dots: false,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+    });
+    $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+    });
+    $('.popup-with-title').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+    });
+    $('.popup-youtube').magnificPopup({
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+});
